4cc1efb6892bfb8baa85abf0a29ef087844e4e4b
Donncha Finlay <donncha.finlay@gmail.com>
2015-06-09 17:19:59 +0200

    Les 5+ Basic Navigation Drawer set up

--- app/src/main/java/dcf/matcom/matdesignsnerd/MainActivity.java
+++ app/src/main/java/dcf/matcom/matdesignsnerd/MainActivity.java
@@ -1,6 +1,7 @@
 package dcf.matcom.matdesignsnerd;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -30,6 +31,11 @@ public class MainActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        //
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //
+        NavigationDrawerFragments drawerFragment = (NavigationDrawerFragments) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setup((DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
     }
 
 
---
+++ app/src/main/java/dcf/matcom/matdesignsnerd/NavigationDrawerFragments.java
@@ -0,0 +1,51 @@
+package dcf.matcom.matdesignsnerd;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragments extends Fragment {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+
+    public NavigationDrawerFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setup( DrawerLayout dl,Toolbar toolbar) {
+        mDrawerLayout = dl;
+        mDrawerToggle =new ActionBarDrawerToggle(getActivity(),dl,toolbar,R.string.drawer_open,R.string.drawer_closed){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+}
